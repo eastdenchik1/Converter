21a56ad8e7d0ff90ba973ef973c26324c79f6467
eastdenchik1 <den030397@gmail.com>
2019-05-05 00:25:37 +0300

    First commit from VS code

---
+++ Converter.java
@@ -0,0 +1,17 @@
+
+/**
+ * Converter
+ */
+
+import java.math.BigInteger;
+import java.util.Random;
+
+public class Converter {
+
+    public static void main(String[] args) {
+        BigInteger num = new BigInteger("Z", 36);
+        BigInteger b = new BigInteger(new BigInteger("8").toString(2), 36);
+        System.out.println(num);
+        System.out.println(b);
+    }
+}
