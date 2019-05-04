544e1570bb4125b4f4a30d09f8334d557680b5f1
eastdenchik1 <den030397@gmail.com>
2019-05-05 02:07:36 +0300

    UPDATE 2.0

--- Converter.java
+++ Converter.java
@@ -15,15 +15,21 @@ public class Converter {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        String number;
+        int srcBase, dstBase;
 
         System.out.println("Введите основание исходного числа:");
-        int srcBase = sc.nextInt();
+        srcBase = sc.nextInt();
 
         System.out.println("Введите основание итогового числа:");
-        int dstBase = sc.nextInt();
+        dstBase = sc.nextInt();
 
         System.out.println("Введите число для перевода:");
-        String number = String.valueOf(sc.nextInt());
+        if (srcBase >= 16 && srcBase <= 36) {
+            number = sc.next();
+        } else {
+            number = String.valueOf(sc.nextInt());
+        }
 
         System.out.println("Исходная система " + srcBase);
         System.out.println("Итоговая система " + dstBase);
